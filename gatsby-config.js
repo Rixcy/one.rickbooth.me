663e314c241c4bdd5505222ec1acc33fc2808a07
Rick Booth <richard.booth@lead-tech.co.uk>
2019-04-08 16:11:20 +0100

    Add favicon and update site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Rick Booth`,
+    description: `My personal portfolio built using Gatsby.`,
     author: `@rixcy`,
   },
   plugins: [
@@ -38,7 +38,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
--- src/components/job.js
+++ src/components/job.js
@@ -6,7 +6,7 @@ export default props => (
     <div className="flex mb-8">
       <div className="flex-1">
         <p className="font-primary font-bold">{props.job.title}</p>
-        <p className="mb-2 font-secondary">{props.job.location}</p>
+        <p className="mb-2 font-secondary">{props.job.company}</p>
         <p className="font-secondary">{props.job.role}</p>
       </div>
       <hr/>
